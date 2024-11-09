108470320d4809fa2b4e7e8697772067a82797fd
dev8 <dev8.sofona@gmail.com>
2024-11-09 20:27:14 +0200

    log

--- app/public/web/index.php
+++ app/public/web/index.php
@@ -3,4 +3,9 @@
 include '../../load.php';
 include '../routes.php';
 
-(new \light\http\Http)->run($_GET['action'] ?? '');
+try {
+    (new \light\http\Http)->run($_GET['action'] ?? '');
+} catch (\Exception $e) {
+    $date = date('Y-m-d');
+    file_put_contents("/var/www/runtime/logs/error-{date}.log", print_r($e->getMessage(), 1) . PHP_EOL, FILE_APPEND);
+}
